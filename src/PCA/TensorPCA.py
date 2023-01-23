c9e71a0bb0a7bec182be954fccc51670b2bb0024
GMX\070038649 <segilber@ca.ibm.com>
2023-01-23 14:53:11 -0500

    src\PCA\TensorPCA.py: __init__(): .view(-1) -> .reshape(-1)

--- src/PCA/TensorPCA.py
+++ src/PCA/TensorPCA.py
@@ -1,13 +1,14 @@
 import numpy as np
-import PCA.PCAModel.PCAModel as PCAModel
+from PCA.PCAModel import PCAModel
 #import PCA.ImagePCAModel as ImagePCAModel
 import torch
 from functools import reduce
 from operator import mul
+import logging
 
 class Model:
     def __init__(self, tensors_list, zeroThreshold=0.000001):
-        # Create single channel arrays with c times the height, where c in the number of channels
+        # Create single channel arrays with length equal to the product of the dimensions
         if len(tensors_list) == 0:
             raise ValueError('TensorPCA.Model.__init__(): The list of tensors is empty')
         self.shape = tensors_list[0].shape
@@ -19,13 +20,15 @@ class Model:
                     f"TensorPCA.Model.__init__(): A tensor has shape {t.shape} while we expect {self.shape}")
 
         stacked_array = np.zeros((len(tensors_list), reduce(mul, self.shape)), dtype=float)
+        #logging.debug(f"TensorPCA.Model(): stacked_array.shape = {stacked_array.shape}")
+        
         for row in range(len(tensors_list)):
-            stacked_array[row, :] = tensors_list[row].view(-1)  # Flatten the tensor
+            stacked_array[row, :] = tensors_list[row].reshape(-1)  # Flatten the tensor
 
         self.pca_model = PCAModel(stacked_array, zeroThreshold)
 
     def Reshape(self, vector):
-        if vector.shape != reduce(mul, self.shape):
+        if vector.shape != (reduce(mul, self.shape), ):
             raise ValueError(f"TensorPCA.Model.Reshape(): vector.shape ({vector.shape}) != reduce(mul, self.shape) ({reduce(mul, self.shape)})")
         if type(vector) is torch.Tensor:
             return vector.view(self.shape)
@@ -35,10 +38,11 @@ class Model:
             raise NotImplementedError(f"TensorPCA.Model.Reshape(): Not implemented type '{type(vector)}'")
 
     def Average(self):
-        return self.Reshape(self.average)
+        return self.Reshape(self.pca_model.average)
 
     def Eigenpairs(self):
         eigenpairs_list = []
-        for eigenvalue, eigenvector in self.eigenpairs:
+        for eigenvalue, eigenvector in self.pca_model.eigenpairs:
             eigen_tsr = torch.tensor(self.Reshape(eigenvector))
             eigenpairs_list.append((eigenvalue, eigen_tsr))
+        return eigenpairs_list
